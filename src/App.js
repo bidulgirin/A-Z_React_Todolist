32b15628eacce7d3d477b927ec8cf9aa455e25f4
bidulgirin <de05164@naver.com>
2022-08-18 01:05:54 +0900

    setState 랑 목록 추가 함수

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 
+// 클래스 컴포넌트
 export default class App extends Component {
+  //State : 상태 저장
+  state = {
+    todoData: [
+      {
+        id: 1,
+        title: "공부하기",
+        complete: false,
+      },
+      {
+        id: 2,
+        title: "청소하기",
+        complete: false,
+      },
+    ],
+    value: "",
+  };
+  //  x버튼 스타일
   btnStyle = {
     color: "#fff",
     border: "none",
@@ -9,6 +27,7 @@ export default class App extends Component {
     cursor: "pointer",
     float: "right",
   };
+  // 목록 스타일
   // 함수형태로 만드는 이유 : 나중에 동적인 변경을 해야해서
   getStyle = () => {
     return {
@@ -18,23 +37,35 @@ export default class App extends Component {
     };
   };
 
-  todoData = [
-    {
-      id: 1,
-      title: "공부하기",
-      complete: true,
-    },
-    {
-      id: 2,
-      title: "청소하기",
-      complete: true,
-    },
-  ];
-
+  // x버튼 클릭 이벤트
   handleClick = (id) => {
-    let newTodoData = this.todoData.filter((data) => (data.id = !id)); // id가 data.id 와 같지 않은 애들만 새로 배열을 만들겠다
-    console.log("살았다", newTodoData);
+    //filter 메서드로 id랑 data.id가 같은 애만 빼고 배열 출력하도록 하는 명령을 변수에저장
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    // console.log('newTodoData', newTodoData)
+    this.setState({ todoData: newTodoData }); //setState() 상태 변화
   };
+  // 글 쓸수 있게 하는 이벤트 핸들러
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+  // 글을 쓰고 submit버튼을 누르면 동작하게 하는 이벤트
+  handleSubmit = (e) => {
+    //새로 고침 안되게
+    e.preventDefault();
+
+    // 새로운 할일 데이터
+    let newTodo = {
+      id: Date.now(), //유니크한 특별한 값을 넣어야해서 이 메서드를 쓰는 거래!
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 할일 추가
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+    //...this.state.todoData 는 원래 있던 데이터를 넣은것이고 (전개연산자로)
+    // newTodo 는 새로 추가되는 데이터이다
+  };
+  // 화면 출력
   render() {
     return (
       <div className="container">
@@ -42,8 +73,24 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-          {/* TODO: map메소드를 이용해 내용의 중복없이 data배열의 나열을 할수있다*/}
-          {this.todoData.map((data) => (
+          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력하세요."
+              onChange={this.handleChange}
+              value={this.state.value}
+            />
+            <input
+              type="submit" //애가 눌려지면 form에 onSubmit가 실행됨
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
+          {/* TODO: map() 메소드를 이용해 내용의 중복없이 data배열을 나열 할 수있다 */}
+          {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={data.complete} />
               {data.title}
