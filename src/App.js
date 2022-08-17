66a293301ac5f14773e688de9d40e72d816d71cf
bidulgirin <de05164@naver.com>
2022-08-17 22:04:24 +0900

    filter

--- src/App.js
+++ src/App.js
@@ -30,6 +30,11 @@ export default class App extends Component {
       complete: true,
     },
   ];
+
+  handleClick = (id) => {
+    let newTodoData = this.todoData.filter((data) => (data.id = !id)); // id가 data.id 와 같지 않은 애들만 새로 배열을 만들겠다
+    console.log("살았다", newTodoData);
+  };
   render() {
     return (
       <div className="container">
@@ -37,12 +42,19 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
-          {/*map메소드를 이용해 내용의 중복없이 data배열의 나열을 할수있다*/}
+          {/* TODO: map메소드를 이용해 내용의 중복없이 data배열의 나열을 할수있다*/}
           {this.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={data.complete} />
               {data.title}
-              <button style={this.btnStyle}>X</button>
+              <button
+                style={this.btnStyle}
+                onClick={() => {
+                  this.handleClick(data.id); //고유한 값을 인자로 해주는 함수를 연결
+                }}
+              >
+                X
+              </button>
             </div>
           ))}
         </div>
